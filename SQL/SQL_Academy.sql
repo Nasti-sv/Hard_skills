65e985c190406f657251fabf5fd0e6fda708d839
Anastasiya Savchuk <nastyasavchuk9709@gmail.com>
2021-12-12 16:58:58 +0200

    Update SQL_Academy.sql

--- SQL/SQL_Academy.sql
+++ SQL/SQL_Academy.sql
@@ -1,3 +1,7 @@
+--Display all customers names listed in our table, with the exception of the one with ID 5.
+SELECT * FROM customers
+WHERE id !=5; 
+
 --11. Вывести пассажиров с самым длинным именем
 SELECT name from Passenger
-ORDER BY LENGTH(name) DESC LIMIT 1
+ORDER BY LENGTH(name) DESC LIMIT 1;
