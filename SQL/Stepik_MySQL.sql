32ef90b716ac4be1fa7b9ca59918e28424045eaf
Anastasiya Savchuk <nastyasavchuk9709@gmail.com>
2022-01-21 21:48:57 +0200

    Update Stepik_MySQL.sql

--- SQL/Stepik_MySQL.sql
+++ SQL/Stepik_MySQL.sql
@@ -104,5 +104,15 @@ FROM book
 GROUP BY book_id, author
 HAVING Порядковый_номер > 2;
 
+--15. Вывести информацию (автора, название и цену) о  книгах, цены которых меньше или равны средней цене книг на складе. 
+--Информацию вывести в отсортированном по убыванию цены виде. Среднее вычислить как среднее по цене книги. 
+SELECT author,title,price
+FROM book
+WHERE price <= (
+         SELECT AVG(price) 
+         FROM book)
+ORDER BY price DESC;
+
+
 
 
