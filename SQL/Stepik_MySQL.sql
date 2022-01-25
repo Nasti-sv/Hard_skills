d79be3f05ddcedbfa155bd7bb39e24cfdf1cc761
Anastasiya Savchuk <nastyasavchuk9709@gmail.com>
2022-01-25 19:44:44 +0200

    Update Stepik_MySQL.sql

--- SQL/Stepik_MySQL.sql
+++ SQL/Stepik_MySQL.sql
@@ -112,7 +112,7 @@ WHERE price <= (
          FROM book)
 ORDER BY price DESC;
 
---16. 
+--16. Вывести информацию (автора, название и цену) о тех книгах, цены которых превышают минимальную цену книги на складе не более чем на 150 рублей в отсортированном по возрастанию цены виде.
 SELECT author, title, price
 FROM book
 WHERE price <= (SELECT MIN(price)+150
