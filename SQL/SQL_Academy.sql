b113a7f54fd7ab7db6b225bf3329afba20440682
Anastasiya Savchuk <nastyasavchuk9709@gmail.com>
2022-02-05 18:20:25 +0200

    Update SQL_Academy.sql

--- SQL/SQL_Academy.sql
+++ SQL/SQL_Academy.sql
@@ -5,3 +5,8 @@
 --11. Вывести пассажиров с самым длинным именем
 SELECT name from Passenger
 ORDER BY LENGTH(name) DESC LIMIT 1;
+
+--14. select distinct town_to from Trip
+join Pass_in_trip on Pass_in_trip.trip = Trip.id
+join Passenger on Passenger.id = Pass_in_trip.passenger
+where Passenger.name like '%Bruce Willis%';
