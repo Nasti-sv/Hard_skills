7da508904059d8a57ff7c4a6f5259fd1db48223f
Anastasiya Savchuk <nastyasavchuk9709@gmail.com>
2022-02-05 18:21:02 +0200

    Update SQL_Academy.sql

--- SQL/SQL_Academy.sql
+++ SQL/SQL_Academy.sql
@@ -6,7 +6,8 @@
 SELECT name from Passenger
 ORDER BY LENGTH(name) DESC LIMIT 1;
 
---14. select distinct town_to from Trip
+--14. Задание 14 В какие города летал Bruce Willis
+select distinct town_to from Trip
 join Pass_in_trip on Pass_in_trip.trip = Trip.id
 join Passenger on Passenger.id = Pass_in_trip.passenger
 where Passenger.name like '%Bruce Willis%';
