4b172b898593f6a22b2cde1bc5dae1097f06fb62
Anastasiya Savchuk <nastyasavchuk9709@gmail.com>
2023-01-11 14:28:58 +0200

    Update w3school.sql

--- SQL/w3school.sql
+++ SQL/w3school.sql
@@ -1,23 +1,23 @@
-1.	Insert the missing statement to get all the columns from the Customers table.
+--1.	Insert the missing statement to get all the columns from the Customers table.
 SELECT * FROM Customers;
-2.	Write a statement that will select the City column from the Customers table.
+--2.	Write a statement that will select the City column from the Customers table.
 SELECT City  FROM Customers;
-3.	Select all the different values from the Country column in the Customers table.
+--3.	Select all the different values from the Country column in the Customers table.
 SELECT DISTINCT Country FROM Customers;
-4.	Select all records where the City column has the value "Berlin".
+--4.	Select all records where the City column has the value "Berlin".
 SELECT * FROM Customers
 WHERE City = “Berlin”;
-5.	Use the NOT keyword to select all records where City is NOT "Berlin".
+--5.	Use the NOT keyword to select all records where City is NOT "Berlin".
 SELECT * FROM Customers
 WHERE NOT City = “Berlin”;
-6.	Select all records where the CustomerID column has the value 32.
+--6.	Select all records where the CustomerID column has the value 32.
 SELECT * FROM Customer
 WHERE CustomerID = 32;
-7.	Select all records where the City column has the value 'Berlin' and the PostalCode column has the value 12209.
+--7.	Select all records where the City column has the value 'Berlin' and the PostalCode column has the value 12209.
 SELECT * FROM Customers
 WHERE City = 'Berlin'
 AND PostalCode = 12209;
-8.	Select all records where the City column has the value 'Berlin' or 'London'.
+--8.	Select all records where the City column has the value 'Berlin' or 'London'.
 SELECT * FROM Customers
 WHERE City = “Berlin”
 OR City = “London”;
