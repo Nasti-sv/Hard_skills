a3b239aed4faac20fc1169ffd6d2a0fa7050a01d
Anastasiya Savchuk <nastyasavchuk9709@gmail.com>
2023-02-04 20:36:21 +0200

    Update w3school.sql

--- SQL/w3school.sql
+++ SQL/w3school.sql
@@ -60,7 +60,16 @@ WHERE PostalCode IS NULL
 SELECT * FROM Customers
 WHERE PostalCode IS NOT NULL;
 
+--SQL Joins
+--Insert the missing parts in the JOIN clause to join the two tables Orders and Customers, 
+--using the CustomerID field in both tables as the relationship between the two tables.
+SELECT * FROM Orders
+LEFT JOIN Customers on Orders.CustomerID = Customers.CustomerID;
 
+--Choose the correct JOIN clause to select all records from the two tables where there is a match in both tables.
+SELECT * FROM Orders
+INNER JOIN Customers
+ON Orders.CustomerID=Customers.CustomerID;
 
 
 
